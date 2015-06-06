50ce3167b4bfd09353f7cef97b7d611b89483141
Denver Abelarde <denver@mobilemo.com>
2015-06-07 02:33:53 +0800

    Added reside menu library for map categories.

--- Bathala-android/app/src/main/java/com/staticobjx/bathala/MainActivity.java
+++ Bathala-android/app/src/main/java/com/staticobjx/bathala/MainActivity.java
@@ -5,18 +5,52 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.special.ResideMenu.ResideMenu;
+import com.special.ResideMenu.ResideMenuItem;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 
 public class MainActivity extends Activity {
 
 
+    @InjectView(R.id.parent_layout)
+    RelativeLayout parentLayout;
+    GoogleMap myMap;
+    MapFragment mapFragment;
+    ResideMenu resideMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+        resideMenu = new ResideMenu(this);
+        resideMenu.setBackground(R.drawable.menu_background);
+        mapFragment = ((MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map));
+        resideMenu.attachToActivity(this);
+        myMap = mapFragment.getMap();
+        // create menu items;
+        String titles[] = {"All", "Events", "Disaster", "Festival", "Others"};
+        int icon[] = {R.drawable.ic_launcher, R.drawable.ic_launcher, R.drawable.ic_launcher, R.drawable.ic_launcher, R.drawable.ic_launcher};
+
+        for (int i = 0; i < titles.length; i++) {
+            ResideMenuItem item = new ResideMenuItem(this, icon[i], titles[i]);
+            resideMenu.addMenuItem(item, ResideMenu.DIRECTION_RIGHT); // or  ResideMenu.DIRECTION_RIGHT
+        }
+        resideMenu.addIgnoredView(parentLayout);
     }
 
+    public void openCategories(View view) {
+        resideMenu.openMenu(ResideMenu.DIRECTION_RIGHT);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
